8bae98f41925b8ddce2c890eb2b1560705b3e7ba
Andrew Brown <abrown747@gmail.com>
2016-10-02 05:58:31 -0700

    Donate button working

--- app/src/main/java/com/example/andrew/cccc/SecondFragment.java
+++ app/src/main/java/com/example/andrew/cccc/SecondFragment.java
@@ -4,13 +4,17 @@ package com.example.andrew.cccc;
  * Created by Andrew on 10/1/2016.
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements View.OnClickListener{
+    Button donateButton;
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle
@@ -19,6 +23,23 @@ public class SecondFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.nav_second_fragment,
                 container, false);
+
+        donateButton = (Button)view.findViewById(R.id.donateButton);
+        donateButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.donateButton){
+            goToUrl("http://chandlerfoodbank.org/causes/donate/");
+        }
+    }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
